f74af801d1e0962bc3046f1350f7331888da2232
zfc <519225281@qq.com>
2019-12-14 16:27:08 +0800

    [!]MyBatis Generator使用过程中踩过的一个坑 https://juejin.im/post/5d107037e51d45599e019de8

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/model/PmsBrand.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/model/PmsBrand.java
@@ -9,25 +9,30 @@ public class PmsBrand implements Serializable {
 
     private String name;
 
-    private static final long serialVersionUID = 1L;
-
-    private Integer sort;
     @ApiModelProperty(value = "首字母")
     private String firstLetter;
 
-    private Integer showStatus;
+    private static final long serialVersionUID = 1L;
+
     @ApiModelProperty(value = "是否为品牌制造商：0->不是；1->是")
     private Integer factoryStatus;
+    private Integer sort;
+
     @ApiModelProperty(value = "产品数量")
     private Integer productCount;
+
     @ApiModelProperty(value = "产品评论数量")
     private Integer productCommentCount;
+
     @ApiModelProperty(value = "品牌logo")
     private String logo;
+
     @ApiModelProperty(value = "专区大图")
     private String bigPic;
+
     @ApiModelProperty(value = "品牌故事")
     private String brandStory;
+    private Integer showStatus;
 
     public Long getId() {
         return id;
